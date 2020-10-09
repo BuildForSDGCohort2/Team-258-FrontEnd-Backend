c181cf2cafacdb4bf6275abac526320e3c90a0d9
briansipula <bsipula@gmail.com>
2020-10-09 08:41:57 +0200

    Deploy Update

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -9,7 +9,7 @@ export const Transaction = ({ transaction }) => {
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text} <span>{sign}${numberWithCommas(Math.abs(transaction.amount))}</span><button onClick={() => deleteTransaction(transaction._id)} className="delete-btn">x</button>
+      {transaction.text} <span>{sign}K{numberWithCommas(Math.abs(transaction.amount))}</span><button onClick={() => deleteTransaction(transaction._id)} className="delete-btn">x</button>
     </li>
   )
 }
--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.use('/api/v1/transactions', transactions);
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
